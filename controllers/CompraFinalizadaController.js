cf96fe468c30dde900c3a57da791a15fc97d50ad
Ricardo Ramos <ricardo.ramos@daltum.mx>
2023-08-31 12:03:47 -0600

    compra

--- controllers/CompraFinalizadaController.js
+++ controllers/CompraFinalizadaController.js
@@ -1678,7 +1678,7 @@ export default{
                  console.log('Socio de negocio:'+ constBussinessPartner.sn_cardcode )
                  console.log('hola vendedor:'+constBussinessPartner.sn_vendedor_codigo_sap);
                  console.log('vendedor info',constUsuarioSellerID.usu_correo_electronico)
-                await ordenCreadaUsuarioDielsaEmail(constCompraFinalizada.dataValues.cf_compra_finalizada_id,);
+                await ordenCreadaUsuarioDielsaEmail(constCompraFinalizada.dataValues.cf_compra_finalizada_id);
 
 
                 
