13a3c65dd72e67f0d62d6511cf751dad2d46fa3d
Ricardo Ramos <ricardo.ramos@daltum.mx>
2023-09-28 15:32:39 -0600

    comit

--- services/registerNewClientEmail.js
+++ services/registerNewClientEmail.js
@@ -108,7 +108,7 @@ exports.registerNewClientEmail = function (data) {
         <section style='background: white; width: 90%; max-width: 800px; margin: 20px auto; text-align: -webkit-center; margin-top: 50px;'>
             <header>
               <div style='color: #0B3196; font-size: 28px; font-weight: 500; letter-spacing: 0; '>
-                <h1>¡Se ha efectuado un nuevo registro!</h1>                
+                <h1>¡Se ha efectuado un nuevo registro (Prospecto)!</h1>                
               </div>
             </header>
             
@@ -124,7 +124,7 @@ exports.registerNewClientEmail = function (data) {
                       <div class="col-sm">
                           <p style="text-align: center;">
                               <h4> ` +
-    data.up_nombre_comercial +
+    (data.up_nombre_comercial != 'null' && data.up_nombre_comercial !='null') ? data.up_nombre_comercial : '' +
     ` </h4>
                           </p>
                       </div>
@@ -137,34 +137,34 @@ exports.registerNewClientEmail = function (data) {
                       <div class="col-sm"></div>
                       <div class="col-sm">
                           <p><strong>Razón social: </strong>` +
-    data.up_razon_social +
+    (data.up_razon_social != null && data.up_razon_social !='null') ? data.up_razon_social : '' +
     `</p>
                           <p><strong>RFC: </strong>` +
-    data.up_rfc +
+    (data.up_rfc != null && data.up_rfc!='null') ? data.up_rfc :'' +
     `</p>
                           <p><strong>Sitio web: </strong>` +
-    data.up_datos_b2b.up_sitio_web +
+    (data.up_datos_b2b.up_sitio_web != null && data.up_datos_b2b.up_sitio_web !='null')? data.up_datos_b2b.up_sitio_web : '' +
     `</p>
                           <p><strong>Cfdi: </strong>` +
-    data.up_cfdi +
+    (data.up_cfdi != null && data.up_cfdi  !='null')? data.up_cfdi : '' +
     `</p>
                           <p><strong>Dirección de facturación: </strong>` +
-    data.up_direccion_facturacion +
+    (data.up_direccion_facturacion  !='null' && data.up_direccion_facturacion!= null) ? data.up_direccion_facturacion:''  +
     `</p>
                           <p><strong>Correo de facturación: </strong>` +
-    data.up_email_facturacion +
+    (data.up_email_facturacion!='null' &&data.up_email_facturacion !=null)? data.up_email_facturacion:''  +
     `</p>
                           <p><strong>Forma de pago: </strong>` +
-    data.up_datos_b2b.up_forma_pago +
+    (data.up_datos_b2b.up_forma_pago!='null' && data.up_datos_b2b.up_forma_pago != null) ? data.up_datos_b2b.up_forma_pago :'' +
     `</p>
                           <p><strong>Medio de pago: </strong>` +
-    data.up_datos_b2b.up_medio_pago +
+    (data.up_datos_b2b.up_medio_pago!='null' && data.up_datos_b2b.up_medio_pago!=null )? data.up_datos_b2b.up_medio_pago :''+
     `</p>
                           <p><strong>Banco: </strong>` +
-    data.up_datos_b2b.up_nombre_banco +
+    (data.up_datos_b2b.up_nombre_banco!='null' && data.up_datos_b2b.up_nombre_banco != null)? data.up_datos_b2b.up_nombre_banco: '' +
     `</p>
                           <p><strong>No. cuenta: </strong>` +
-    data.up_datos_b2b.up_numero_cuenta_banco +
+    (data.up_datos_b2b.up_numero_cuenta_banco!='null' && data.up_datos_b2b.up_numero_cuenta_banco!= null)? data.up_datos_b2b.up_numero_cuenta_banco :'' +
     `</p>                          
                       </div>
                       <div class="col-sm"></div>
