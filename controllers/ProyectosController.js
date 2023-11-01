0884eaaf03749bf8e7d3cd8faa2574322d6dab86
Ricardo Ramos <ricardo.ramos@daltum.mx>
2023-11-01 17:30:08 -0600

    prueba resultados productos

--- controllers/ProyectosController.js
+++ controllers/ProyectosController.js
@@ -85,10 +85,12 @@ export default {
             let dataLineasProyecto = [];
             if(dataProyecto){
                 dataLineasProyecto = await sequelize.query(`
-                    SELECT lpro.*, pro.prod_nombre_extranjero, img.imgprod_nombre_archivo, img.imgprod_ruta_archivo FROM lineas_proyectos AS lpro
+                    SELECT Top(1) lpro.*, pro.prod_nombre_extranjero, img.imgprod_nombre_archivo, img.imgprod_ruta_archivo FROM lineas_proyectos AS lpro
                     LEFT JOIN productos AS pro ON pro."prod_sku" = lpro."codigoArticulo"
                     LEFT JOIN imagenes_producto AS img ON img.imgprod_prod_producto_id = pro.prod_producto_id
-                    WHERE lpro."idProyecto" = ${dataProyecto.dataValues.id};`,
+                    WHERE lpro."idProyecto" = ${dataProyecto.dataValues.id};
+                    limit 1
+                    `,
                 {
                     type: sequelize.QueryTypes.SELECT 
                 });
