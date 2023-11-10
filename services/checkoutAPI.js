3892f9a24eef16b068406827e28298e753f884ca
Ricardo Ramos <ricardo.ramos@daltum.mx>
2023-11-09 20:37:59 -0600

    prueba

--- services/checkoutAPI.js
+++ services/checkoutAPI.js
@@ -1152,7 +1152,7 @@ module.exports = {
             {
                 //Ver que estamos filtrando
                 let cardK = String(constProductoCarritoDeCompra[z].dataValues.pcdc_prod_producto_id)
-                    pruebaTester(cardK +' : ' + constProductoCarritoDeCompra[z].dataValues.producto.prod_precio)
+                   // pruebaTester(cardK +' : ' + constProductoCarritoDeCompra[z].dataValues.producto.prod_precio)
                    
                 //Consultar tabla productos stock general por producto
                 const constTieneStockGeneral = await models.Producto.findOne(
