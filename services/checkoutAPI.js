55a5e4aeeb7971d88451e25a547c0daa5f08ce47
Ricardo Ramos <ricardo.ramos@daltum.mx>
2023-11-29 13:08:44 -0600

    pruebas

--- services/checkoutAPI.js
+++ services/checkoutAPI.js
@@ -1162,7 +1162,7 @@ module.exports = {
                  constProductoCarritoDeCompra[s].dataValues.producto.prod_precio = (newProductProyect && (newProductProyect.precio < constProductoCarritoDeCompra[s].dataValues.producto.prod_precio || constProductoCarritoDeCompra[s].dataValues.producto.prod_precio ===0)? Number(newProductProyect.precio*USDValor) : constProductoCarritoDeCompra[s].dataValues.producto.prod_precio );
 
                  constProductoCarritoDeCompra[s].dataValues.producto.prod_precioOriginal = (newProductProyect && (newProductProyect.precio < constProductoCarritoDeCompra[s].dataValues.producto.prod_precio || constProductoCarritoDeCompra[s].dataValues.producto.prod_precio ===0)? Number(newProductProyect.precio*USDValor) : constProductoCarritoDeCompra[s].dataValues.producto.prod_precio );
-                 //pruebaTester(String(constProductoCarritoDeCompra[s].dataValues.producto.prod_precioOriginal))
+                 pruebaTester(String(constProductoCarritoDeCompra[s].dataValues.producto.prod_precioOriginal))
                 //pruebaTester( constProductoCarritoDeCompra[s].dataValues.producto.prod_precio + ' sku:' + constProductoCarritoDeCompra[s].dataValues.producto.prod_sku)
                newProductsProyects.push(constProductoCarritoDeCompra[s])
            }
